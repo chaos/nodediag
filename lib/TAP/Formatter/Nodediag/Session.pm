4156022a72b2625dad88ab86d6ec9cdc32acee16
Jim Garlick <garlick.jim@gmail.com>
2011-10-19 11:49:38 -0700

    Fix nodeattr -vx (issue 8)
    In addition:
    - avoid global %conf reference from TAP::Formatter::Nodediag::Session
      that probably breaks prove --formatter TAP::Formatter::Nodediag
    - honor --firstfail in --quiet mode
    - add missing --failonly description to man page

--- lib/TAP/Formatter/Nodediag/Session.pm
+++ lib/TAP/Formatter/Nodediag/Session.pm
@@ -34,26 +34,44 @@ sub close_test {
 
     $name =~ s/\.t$//; # drop the .t suffix from test name
 
-    return if $formatter->really_quiet;
-    return if $formatter->quiet && !$parser->has_problems;
-
-    $formatter->_output (sprintf "%-60s", "Checking $name:" );
-    if ( $parser->has_problems ) {
-        $formatter->_output_color ( 'red',    "[ FAIL ]\n");
-    } elsif ( $parser->skip_all || $parser->skipped > 0) {
-        $formatter->_output_color ( 'yellow', "[NOTRUN]\n" );
-    } else {
-        $formatter->_output_color ( 'green',  "[  OK  ]\n");
+    # Harness defined verbosity values are:
+    #  1:  verbose, add raw test results
+    #  0:  normal, show invividual test results
+    # -1:  quiet, show only failed individual test results
+    # -2:  really_quiet, suppress all but summary
+    # -3:  silent, suppress everything
+    # We have to respect these values so we can run under prove.
+    # However we do sneak in some flags (avoiding bitwise ops since we have
+    # a signed integer) that allow nodeattr -vx (verbose fail only) and
+    # nodeattr -f (exit on first fail).
+    #
+    my $verbosity = $formatter->verbosity;
+    my $firstfail = $verbosity >= 0x20 - 3 ? 1 : 0;
+    $verbosity -= 0x20 if $firstfail;
+    my $verbose = $verbosity >= 0x10 - 3 ? 1 : 0;
+    $verbosity -= 0x10 if $verbose;
+    
+    if ($verbosity > -2) {
+        if ($verbosity > -1 || $parser->has_problems) {
+            $formatter->_output (sprintf "%-60s", "Checking $name:" );
+            if ( $parser->has_problems ) {
+                $formatter->_output_color ( 'red',    "[ FAIL ]\n");
+            } elsif ( $parser->skip_all || $parser->skipped > 0) {
+                $formatter->_output_color ( 'yellow', "[NOTRUN]\n" );
+            } else {
+                $formatter->_output_color ( 'green',  "[  OK  ]\n");
+            }
+            if ($verbosity >= 1 || $verbose) {
+                $formatter->_output ($self->{results} ? $self->{results} :"\n");
+            }
+        }
     }
-
-    # Dump raw output for this test if verbose
-    if ($formatter->verbose) {
-        $formatter->_output ( ( $self->{results} ? $self->{results} : "\n" ) );
-    }
-
-    if ($parser->has_problems && $main::conf{firstfail}) {
+    # Arguably this is wrong but we are fighting Tap::Harness here
+    if ($parser->has_problems && $firstfail) {
         exit 1;
     }
 }
+
+# vi: ts=4 sw=4 expandtab
 
 1;
